8ce7319e13ccbf16fe7437a19412cab1c4c911f7
royel lopez <royerlopeznunez@gmail.com>
2021-06-18 11:24:53 -0500

    repositorio

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+
+import { PokemonReducer } from './app/views/reducer/pokemonReducer';
+
+let reducers = combineReducers({PokemonReducer})
+
+const store = createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunkMiddleware)
+));
+  
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
